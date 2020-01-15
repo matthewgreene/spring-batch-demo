005b5317bf946c7cc5bd78846196557fdd07578b
Ben Turney <bturney@mymove.com>
2020-01-15 10:28:46 -0500

    tests pass now?

--- src/main/java/com/scratch/batch/batchdemo/config/BatchConfig.java
+++ src/main/java/com/scratch/batch/batchdemo/config/BatchConfig.java
@@ -1,87 +1,38 @@
 package com.scratch.batch.batchdemo.config;
 
-import com.scratch.batch.batchdemo.UserItemProcessor;
-import com.scratch.batch.batchdemo.UserItemWriter;
-import com.scratch.batch.batchdemo.user.User;
-import com.scratch.batch.batchdemo.util.ClassUtils;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
-import java.io.File;
-import java.nio.file.Path;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfig {
 
-    private final StepBuilderFactory stepBuilderFactory;
-    private final JobBuilderFactory jobBuilderFactory;
-
-    @Value("${inputFile}")
-    private File inputFile;
-    @Value("${output-directory}")
-    private Path outputDirectory;
-
-    public BatchConfig(StepBuilderFactory stepBuilderFactory, JobBuilderFactory jobBuilderFactory) {
-        this.stepBuilderFactory = stepBuilderFactory;
-        this.jobBuilderFactory = jobBuilderFactory;
-    }
-
     @Bean
-    public FlatFileItemReader<User> userFlatFileItemReader() {
-        return new FlatFileItemReaderBuilder<User>()
-                .name("userItemReader")
-                .resource(new FileSystemResource(inputFile))
-                .delimited()
-                .delimiter(",")
-                .names(ClassUtils.getFieldNames(User.class))
-                .fieldSetMapper(new BeanWrapperFieldSetMapper<User>() {
-                    {
-                        setTargetType(User.class);
-                    }
-                })
+    public Job job(JobBuilderFactory jobs, StepBuilderFactory steps) {
+        return jobs.get("job")
+                .start(steps.get("step")
+                        .tasklet((contribution, chunkContext) -> {
+                            System.out.println("hello world");
+                            return RepeatStatus.FINISHED;
+                        })
+                        .build())
                 .build();
     }
 
     @Bean
-    public ItemProcessor<User, String> userItemProcessor() {
-        return new UserItemProcessor();
-    }
-
-    @Bean
-    public ItemWriter<String> userItemWriter() {
-        return new UserItemWriter();
-    }
-
-    @Bean
-    public Step userStep(FlatFileItemReader<User> reader,
-                         ItemProcessor<User, String> processor,
-                         ItemWriter<String> writer) {
-        return stepBuilderFactory.get("userStep")
-                .<User, String>chunk(50_000)
-                .reader(reader)
-                .processor(processor)
-                .writer(writer)
-                .faultTolerant()
-                .build();
-    }
-
-    @Bean
-    public Job userJob(Step userStep) {
-        return jobBuilderFactory.get("userJob")
-                .start(userStep)
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("/org/springframework/batch/core/schema-h2.sql")
                 .build();
     }
 }
--- src/test/java/com/scratch/batch/batchdemo/config/BatchConfigTest.java
+++ src/test/java/com/scratch/batch/batchdemo/config/BatchConfigTest.java
@@ -1,24 +1,44 @@
 package com.scratch.batch.batchdemo.config;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBatchTest
-//@RunWith(SpringRunner.class)
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = BatchConfig.class)
 class BatchConfigTest {
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
 
+    @Autowired
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @BeforeEach
+    public void clearJobExecutions() {
+        this.jobRepositoryTestUtils.removeJobExecutions();
+    }
+
     @Test
-    void userStep() {
-        assertNotNull(jobLauncherTestUtils, "jobLauncherTestUtils should not be null");
+    public void testMyJob() throws Exception {
+        // given
+        JobParameters jobParameters = this.jobLauncherTestUtils.getUniqueJobParameters();
+
+        // when
+        JobExecution jobExecution = this.jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        Assert.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
     }
 }
