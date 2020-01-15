68f941beb80a6f187dc6baa3b4d5ca0a7fb3e5d9
mgreene <mgreene@redventures.com>
2020-01-15 06:15:39 -0500

    Use @ExtendWith annotation for JUnit5

--- src/test/java/com/scratch/batch/batchdemo/config/BatchConfigTest.java
+++ src/test/java/com/scratch/batch/batchdemo/config/BatchConfigTest.java
@@ -1,21 +1,18 @@
 package com.scratch.batch.batchdemo.config;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.batch.core.Job;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBatchTest
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {BatchConfig.class, BatchConfigTest.TestConfiguration.class})
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = BatchConfig.class)
 class BatchConfigTest {
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
@@ -23,19 +20,5 @@ class BatchConfigTest {
     @Test
     void userStep() {
         assertNotNull(jobLauncherTestUtils, "jobLauncherTestUtils should not be null");
-    }
-
-    @Configuration
-    static class TestConfiguration {
-        @Autowired
-        @Qualifier("userJob")
-        private Job userJob;
-
-        @Bean
-        public JobLauncherTestUtils jobLauncherTestUtils() {
-            JobLauncherTestUtils utils = new JobLauncherTestUtils();
-            utils.setJob(userJob);
-            return utils;
-        }
     }
 }
